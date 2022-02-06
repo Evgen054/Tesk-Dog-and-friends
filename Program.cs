d32ab292b73d366591d6606f7f8bcc39f8742e4c
EVG54 <bankir032@mail.ru>
2022-02-06 17:07:28 +0300

    home worck

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿double speedOneFreand = 5;
+double speedTwoFreand = 10;
+double speedDog = 20;
+double distance = 100;
+double distanceLimit = 10;
+int dogCount = 0;
+int dogDirection = 1;
+double speedOll = 0;
+double time = 0;
+
+while (distance > distanceLimit)
+{
+    if (dogDirection == 1)
+    {
+        speedOll = speedOneFreand + speedDog;
+        time = distance / speedOll;
+        distance = distance - speedOll * time - speedTwoFreand * time;
+        dogDirection = 2;
+        dogCount++;
+    }
+    else
+        speedOll = speedTwoFreand + speedDog;
+    time = distance / speedOll;
+    distance = distance - speedOll * time - speedOneFreand * time;
+    dogDirection = 1;
+    dogCount++;
+}
+Console.WriteLine($"Собака сделала {dogCount} круга");
